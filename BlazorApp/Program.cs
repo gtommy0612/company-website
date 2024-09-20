b17325b968f95a0d7d8255c39401cb06cab5008d
Tommy <gtommy0612@gmail.com>
2024-09-20 19:33:06 +0800

    feat: transform the html format files into Blazor.

---
+++ BlazorApp/Program.cs
@@ -0,0 +1,34 @@
+using BlazorApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// 
+// Add localization
+// builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+// builder.Services.AddRazorPages();
+// builder.Services.AddServerSideBlazor();
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ BlazorApp/Resources/Labels.Designer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Resources;
+
+namespace YourNamespace.Resources  // 替換為實際的命名空間
+{
+    /// <summary>
+    ///   這個類別是用於查找在 Labels.resx 中的字串。
+    /// </summary>
+    public class Labels
+    {
+        private static ResourceManager resourceMan;
+
+        private static CultureInfo resourceCulture;
+
+        /// <summary>
+        ///   返回此類的資源管理器實例。
+        /// </summary>
+        public static ResourceManager ResourceManager
+        {
+            get
+            {
+                if (resourceMan == null)
+                {
+                    resourceMan = new ResourceManager("YourNamespace.Resources.Labels", typeof(Labels).Assembly);
+                }
+                return resourceMan;
+            }
+        }
+
+        /// <summary>
+        ///   使用當前文化的字串。
+        /// </summary>
+        public static CultureInfo Culture
+        {
+            get => resourceCulture;
+            set => resourceCulture = value;
+        }
+
+        /// <summary>
+        ///   返回 Labels.resx 中的字串。
+        /// </summary>
+        public static string Welcome => ResourceManager.GetString("Welcome", resourceCulture);
+        // 添加更多屬性來訪問其他資源字串
+    }
+}
