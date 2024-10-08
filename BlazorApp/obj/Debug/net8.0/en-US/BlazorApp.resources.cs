69597ac7f16eb782cbeb9b6f8c8504591d157fea
Tommy <gtommy0612@gmail.com>
2024-10-08 11:17:06 +0800

    feat: change company phone number.

--- BlazorApp/obj/Debug/net8.0/en-US/BlazorApp.resources.cs
+++ BlazorApp/obj/Debug/net8.0/en-US/BlazorApp.resources.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute(" ")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1187e14a188fc2f5f50fd93687b2dae3517715e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+399831416414108379860dd224aa90dd81aad5e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
