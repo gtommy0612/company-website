3c885fdf8944330fa032f7f54621bbe768b10231
Tommy <gtommy0612@gmail.com>
2024-11-22 15:37:54 +0800

    no message

--- BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69597ac7f16eb782cbeb9b6f8c8504591d157fea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee417d52ee67a20f025639b1f1208e735ca2fb95")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
